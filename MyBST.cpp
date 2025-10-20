7a857ecbc3772e6edcd2bf1920ee8046534cba06
Tellez05 <A01640907@tec.mx>
2025-10-19 20:16:46 -0600

    El pepe

--- MyBST.cpp
+++ MyBST.cpp
@@ -1,15 +1,15 @@
 #include "MyBST.h"
 
 MyBST::MyBST(){
-    this->root = nullptr; 
-    size = 0; 
+    this->root = nullptr;   
 }
+
 int MyBST::length(){
     return this->size; 
 }
 
 bool MyBST::isEmpty(){
-    return size==0; 
+    return size == 0; 
 }
 
 bool MyBST::search(int data){
@@ -49,7 +49,110 @@ bool MyBST::insert(int data){
 
 bool MyBST::remove(int data){
     MyNodeBST *current = root; 
+    MyNodeBST *currentatras; 
+    MyNodeBST *currentatras2; 
+    int cont {0};
+
     while(current != nullptr){
+        cont = 0;
+        if(current->data> data){
+            currentatras = current; 
+            current = current->left;
+            cont += 1;  
+        }else if(current->data<data){
+            currentatras = current; 
+            current = current->right;
+        }else{
+            break;
+        }
+    }
+    //caso 0 hijos
+    if(current->left == nullptr && current->right == nullptr){
+        if(cont == 0){
+            currentatras->right = nullptr; 
+        }else{
+            currentatras->left = nullptr; 
+        }
+        delete current;
+        return true; 
+    //Caso 1 Hijo
+    }else if (current->left != nullptr && current->right == nullptr) {
+        currentatras->left = current->left;  
+        delete current; 
+        return true;
+    }else if (current->left == nullptr && current->right != nullptr) {
+        currentatras->right = current->right;
+        delete current; 
+        return true;
+    //Caso 2 hijos
+    }else{
+        current = current->left; 
+        while(current->right != nullptr){
+            currentatras2 = current; 
+            current = current->right; 
+        }
+        currentatras2->right = nullptr; 
+        if(cont == 0){
+            currentatras->right->data = current->data; 
+        }else{
+            currentatras->left->data = current->data; 
+        }
+        delete current; 
+        return true; 
 
     }
+    return false; 
+}
+
+
+
+void MyBST::preorder(){
+    MyNodeBST *current {root};
+    preorder(current); 
+}
+
+void MyBST::preorder(MyNodeBST *current){
+    cout<<current->data<<","; 
+    if(current->left != nullptr){
+        preorder(current->left);
+    }if(current->right != nullptr){
+        preorder(current->right);
+    }
+}
+
+void MyBST::inorder(){
+    MyNodeBST *current {root}; 
+    inorder(current);
+}
+
+void MyBST::inorder(MyNodeBST *current){
+    if(current->left != nullptr){
+        inorder(current->left);
+    }
+    cout<<current->data<<","; 
+    if(current->right != nullptr){
+        inorder(current->right);
+    }
+
+}
+
+void MyBST::postorder(){
+    MyNodeBST *current {root};
+    postorder(current); 
+}
+
+void MyBST::postorder(MyNodeBST *current){
+    if(current->left != nullptr){
+        postorder(current->left);
+    }
+    if(current->right != nullptr){
+        postorder(current->right);
+    }
+    cout<<current->data<<","; 
+}
+
+void MyBST::level(){
+    MyNodeBST* array = new MyNodeBST[size];
+    MyNodeBST* current {root};
+
 }
--- MyBST.h
+++ MyBST.h
@@ -1,5 +1,6 @@
 #pragma once 
 #include <iostream>
+
 using namespace std; 
 
 struct MyNodeBST{
@@ -10,27 +11,38 @@ struct MyNodeBST{
         this->data=data;
         this->left=this->right=nullptr;
     }
+    MyNodeBST(); 
 };
 
 class MyBST{
     private:
-        int size; 
-        MyNodeBST* root; 
-    public: 
-        MyBST(); 
-        int length(); 
-        bool isEmpty(); 
+        int size;
+        MyNodeBST* root;
+        bool search(int data,MyNodeBST* current);
+        void preorder(MyNodeBST* current);
+        void inorder(MyNodeBST* current);
+        void postorder(MyNodeBST* current);
+        //En caso de hacer alguno de los métodos recursivos y necesiten
+        //otro método que haga la recursión deben de colocarlo como privado
+        //sólo la función de preparación sería pública
+    public:
+        MyBST();
+        int length();
+        bool isEmpty();
         bool search(int data);
-        bool insert(int data); 
-        bool remove(int data); 
-        void preorder(); 
-        void inorder(); 
-        void level(); 
-        void visit(int type);
-        int height(); 
-        void ancestors(int data);
-        int whatLevelIAmI(int data); 
-
-
-
+        bool insert(int data);//true si lo inserto o no
+        bool remove(int data);//si el valor estaba en el árbol lo borra
+                              //y regresa true, caso contrario sólo regresa false. En caso de borrar un nodo con dos hijos utilizar el predecesor
+        void preorder();
+        void inorder();
+        void postorder();
+        void level();
+        void visit(int type);//Type: 1->preorder,2->inorder.3->postorder,4->level
+                                        //Imprime el recorrido separado por comas sin espacio entre los elementos.
+        int height();//regresa la altura del BST.
+        void ancestors(int data);//imprime los ancestros del valor pasado separados por comas y sin dejar espacios entre ellos.
+                                 //el orden de impresión debe ser de la raíz hacía abajo.
+                                //Si valor no se localiza en el árbol no imprime nada
+        int whatLevelAmI(int data);//regresa el nivel en que se encuentra el valor
+                                   //dentro del árbol. Si el valor no está en árbol regresa -1
 };
